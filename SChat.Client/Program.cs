5d86eabb641a2ed111350edbf8fab44b98ad594b
Anton Knapp <antonk3344@gmail.com>
2024-09-30 21:23:39 +0300

    Setup client and server

--- SChat.Client/Program.cs
+++ SChat.Client/Program.cs
@@ -1,1 +1,59 @@
-﻿
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+var host = "192.168.1.176";
+var port = 5001;
+var deviceId = Guid.NewGuid();
+
+var ipEndPoint = new IPEndPoint(IPAddress.Parse(host), port);
+
+var stopWord = "exit";
+
+var cancellationToken = new CancellationTokenSource();
+
+using var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+await socket.ConnectAsync(ipEndPoint);
+
+Console.WriteLine($"Successfully connected to {host} (port: {port})");
+
+var readingTask = Task.Factory.StartNew(
+	StartReading, cancellationToken.Token, TaskCreationOptions.LongRunning, TaskScheduler.Default);
+// var writingTask = Task.Factory.StartNew(
+// 	StartWriting, cancellationToken.Token, TaskCreationOptions.LongRunning, TaskScheduler.Default);
+
+await StartWriting();
+
+socket.Shutdown(SocketShutdown.Both);
+
+async Task StartReading()
+{
+	while(true)
+	{
+		var buffer = new byte[1024];
+		var receivedBytes = await socket.ReceiveAsync(buffer);
+
+		if (receivedBytes != 0)
+		{
+			var message = Encoding.UTF8.GetString(buffer, 0, receivedBytes);
+			Console.WriteLine(message);
+		}
+	}
+}
+
+async Task StartWriting()
+{
+	var input = string.Empty;
+	
+	while (true)
+	{
+		input = deviceId + ": " + Console.ReadLine();
+		var buffer = Encoding.UTF8.GetBytes(input!);
+
+		if (input == stopWord)
+			break;
+
+		await socket.SendAsync(buffer);
+	}
+}
--- SChat.Server/Program.cs
+++ SChat.Server/Program.cs
@@ -1,1 +1,64 @@
-﻿
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+var clients = new List<Socket>();
+
+var host = "192.168.1.176";
+var port = 5001;
+
+var cancellationToken = new CancellationTokenSource();
+
+using var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+var ipEndPoint = new IPEndPoint(IPAddress.Any, port);
+
+socket.Bind(ipEndPoint);
+
+socket.Listen(10);
+
+Console.WriteLine("Waiting for clients...");
+
+while (true)
+{
+	Socket clientSocket = socket.Accept();
+	clients.Add(clientSocket);
+
+	var clientTask = Task.Factory.StartNew(
+		() => Handle(clientSocket), cancellationToken.Token, TaskCreationOptions.LongRunning, TaskScheduler.Default);
+}
+
+async Task Handle(Socket clientSocket)
+{
+	var buffer = new byte[1024];
+
+	while (true)
+	{
+		var receivedBytes = await clientSocket.ReceiveAsync(buffer);
+		
+		if (receivedBytes == 0)
+			break;
+
+		string message = Encoding.UTF8.GetString(buffer, 0, receivedBytes);
+		
+		Console.WriteLine($"New message received: {message}");
+
+		BroadcastMessage(message, clientSocket);
+	}
+
+	clients.Remove(clientSocket);
+
+	clientSocket.Shutdown(SocketShutdown.Both);
+	clientSocket.Close();
+}
+
+void BroadcastMessage(string message, Socket sender)
+{
+	var buffer = Encoding.UTF8.GetBytes(message);
+
+	foreach (var client in clients)
+	{
+		if (client != sender)
+			client.Send(buffer);
+	}
+}
